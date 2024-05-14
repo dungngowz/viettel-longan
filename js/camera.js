f358037cdcc113db8a968a80bfdc4fac28a9ac92
dungngowz <122582853+dungngowz@users.noreply.github.com>
2024-05-14 09:57:34 +0700

    Add files via upload

--- js/camera.js
+++ js/camera.js
@@ -6,7 +6,23 @@ const cameras = [
     price: "",
     discountPrice: "990.000 đ",
     desc: "• Độ phân giải Full HD 2Mpx (1920 x 1080) <br/> • Hỗ trợ hồng ngoại lên tới khoảng cách 30m <br/>• Tính năng giảm nhiễu số 3D DNR <br/>• Tính năng chống ngược sáng WDR <br/>...",
-    img: "/img/cam1.png",
+    img: [
+      "/img/cam1.png",
+      "/img/cam1.1.png",
+      "/img/cam1.2.png",
+      "/img/cam1.3.png",
+      "/img/cam1.4.png",
+      "/img/cam1.5.png",
+    ],
+
+    descDetails: `<div data-v-6d96144b="" id="product-desc__content" class="device-description" style="overflow: unset; height: unset;"><p data-v-6d96144b=""></p><p>
+	<span style="font-family: &quot;times new roman&quot;, times, serif; font-size: 16px;">Mức giá <strong>990.000đ</strong>&nbsp;là mức giá tạm tính cho khách hàng mua lẻ thiết bị, không bao gồm phí sim số + phí hòa mạng + phí giao hàng.</span></p>
+<p>
+	<strong><span style="font-size: 16px;"><span style="font-family: &quot;times new roman&quot;, times, serif;">Hiện Camera Viettel đang có chính sách trang bị MIỄN PHÍ camera trong nhà, hoặc trang bị camera ngoài trời với mức phí chỉ 300.000đ cho khánh hàng mua combo FTTH + Camera. (Áp dụng cả với khách hàng đã có FTTH Viettel).</span></span></strong></p>
+<p>
+	<strong><span style="font-size: 16px;"><span style="font-family: &quot;times new roman&quot;, times, serif;">Chi tiết các gói combo FTTH + Camera tham khảo tại link:&nbsp;<a href="https://viettel.vn/internet-truyenhinh/combo">https://viettel.vn/internet-truyenhinh/combo</a>&nbsp;hoặc liên hệ 18008168 (Miễn phí) để được tư vấn.</span></span></strong></p>
+<div class="image-camera-table">
+	<img alt="" src="https://media.vietteltelecom.vn/upload/ckfinder/images/Gia Cloud moi_T32024.PNG" style="width: 663px; height: 125px;"></div>  <p data-v-6d96144b=""></p><div>	<div>		<strong style="font-family: &quot;times new roman&quot;, times, serif; font-size: 16px;">Tính năng nổi bật</strong></div></div><div>	<span style="font-size:16px;"><span style="font-family:times new roman,times,serif;">- Độ phân giải Full HD 2Mpx (1920 x 1080)</span></span></div><div>	<span style="font-size:16px;"><span style="font-family:times new roman,times,serif;">- Hỗ trợ hồng ngoại lên tới khoảng cách 30m</span></span></div><div>	<span style="font-size:16px;"><span style="font-family:times new roman,times,serif;">- Công nghệ nén hình ảnh theo chuẩn H.265</span></span></div><div>	<span style="font-size:16px;"><span style="font-family:times new roman,times,serif;">- Khả năng chống nước, bụi IP67</span></span></div><div>	<span style="font-size:16px;"><span style="font-family:times new roman,times,serif;">- Tích hợp công nghệ AI cho phép phân biệt chuyển động của con người với vật, giảm cảnh báo giả.</span></span></div><div>	<span style="font-size:16px;"><span style="font-family:times new roman,times,serif;">- Hình ảnh quan sát có màu vào ban đêm</span></span></div><div>	<span style="font-size:16px;"><span style="font-family:times new roman,times,serif;">- Tính năng đàm thoại 2 chiều trực tiếp.</span></span></div><div>	<span style="font-size:16px;"><span style="font-family:times new roman,times,serif;">- Có đèn và còi báo động khi phát hiện chuyển động vào ban đêm.</span></span></div><div>	<span style="font-size:16px;"><span style="font-family:times new roman,times,serif;"><strong>Bảo hành</strong>: 12 tháng tại các cửa hàng Viettel</span></span></div><p></p></div>`,
   },
   {
     id: 2,
@@ -15,7 +31,22 @@ const cameras = [
     price: "",
     discountPrice: "690.000 đ",
     desc: "• Độ phân giải Full HD 2Mpx (1920 x 1080) <br/> • Hỗ trợ hồng ngoại lên tới khoảng cách 30m <br/>• Tính năng giảm nhiễu số 3D DNR <br/>• Tính năng chống ngược sáng WDR <br/> ...",
-    img: "/img/cam2.png",
+    img: [
+      "/img/cam2.png",
+      "/img/cam2.1.png",
+      "/img/cam2.2.png",
+      "/img/cam2.3.png",
+      "/img/cam2.4.png",
+    ],
+
+    descDetails: `<div data-v-6d96144b="" id="product-desc__content" class="device-description" style="overflow: unset; height: unset;"><p data-v-6d96144b=""><p>
+	<span style="font-family: &quot;times new roman&quot;, times, serif; font-size: 16px;">Mức giá <strong>690.000đ</strong> là mức giá tạm tính cho khách hàng mua lẻ thiết bị, không bao gồm phí sim số + phí hòa mạng + phí giao hàng.</span></p>
+<p>
+	<strong><span style="font-size:16px;"><span style="font-family:times new roman,times,serif;">Hiện Camera Viettel đang có chính sách trang bị MIỄN PHÍ camera trong nhà, hoặc trang bị camera ngoài trời với mức phí chỉ 300.000đ cho khánh hàng mua combo FTTH + Camera. (Áp dụng cả với khách hàng đã có FTTH Viettel). </span></span></strong></p>
+<p>
+	<strong><span style="font-size:16px;"><span style="font-family:times new roman,times,serif;">Chi tiết các gói combo FTTH + Camera tham khảo tại link: <a href="https://viettel.vn/internet-truyenhinh/combo">https://viettel.vn/internet-truyenhinh/combo</a>&nbsp;hoặc liên hệ 18008168 (Miễn phí) để được tư vấn.</span></span></strong></p>
+<div class="image-camera-table">
+	<img alt="" src="https://media.vietteltelecom.vn/upload/ckfinder/images/Gia Cloud moi_T32024.PNG" style="color: rgb(0, 0, 0); font-family: RlMgUEZCZWFUFucBybyMaWdodCvdGY; font-size: 16px; font-weight: 700; text-align: center; width: 663px; height: 125px;"></div> <p data-v-6d96144b=""><div>	<div>		<strong><span style="font-size:16px;"><span style="font-family:times new roman,times,serif;">Thông số kỹ thuật:</span></span></strong></div></div><div>	<div>		<span style="font-size:16px;"><span style="font-family:times new roman,times,serif;">+ Độ phân giải Full HD 2Mpx (1920 x 1080)</span></span></div>	<div>		<span style="font-size:16px;"><span style="font-family:times new roman,times,serif;">+ Hỗ trợ hồng ngoại lên tới khoảng cách 10m</span></span></div>	<div>		<span style="font-size:16px;"><span style="font-family:times new roman,times,serif;">+ Tính năng đàm thoại 02 chiều (02 way talking)</span></span></div>	<div>		<span style="font-size:16px;"><span style="font-family:times new roman,times,serif;">+ Tự động xoay 360 độ, bám theo mọi chuyển động.</span></span></div>	<div>		<span style="font-size:16px;"><span style="font-family:times new roman,times,serif;">+ Hình ảnh sắc nét, công nghệ nén hình ảnh theo chuẩn H.265</span></span></div>	<div>		<strong><span style="font-size:16px;"><span style="font-family:times new roman,times,serif;">Tính năng mới:</span></span></strong></div>	<div>		<span style="font-size:16px;"><span style="font-family:times new roman,times,serif;">+ Gọi điện khẩn cấp SOS từ Camera lên ứng dụng Viettel Home</span></span></div>	<div>		<span style="font-size:16px;"><span style="font-family:times new roman,times,serif;">+ Công nghệ AI thông minh phân biệt giữa người và vật, giảm cảnh báo giả.</span></span></div>	<div>		<span style="font-size:16px;"><span style="font-family:times new roman,times,serif;">+ Cài đặt Camera báo động bằng nhiều âm thanh khác nhau khi phát hiện người, chuyển động.</span></span></div>	<div>		<span style="font-size:16px;"><span style="font-family:times new roman,times,serif;"><strong>Bảo hành:</strong>12 tháng tại các cửa hàng Viettel</span></span></div></div></p></div>`,
   },
 ];
 
@@ -31,11 +62,11 @@ function renderCamera() {
 
     cameraItem.innerHTML = `
     <div class="bg-white internet-item  wow fadeInUp"
-    
+    onclick="openCameraDetails('${index}')"
         data-wow-delay="${delay}ms"
     >
                     <div style="height: 200px; " class="pt-4">
-                        <img src=${item.img} alt="" width="100%" height="100%" style="object-fit: contain;">
+                        <img src=${item.img[0]} alt="" width="100%" height="100%" style="object-fit: contain;">
                     </div>
 
                     <div class="px-3 pt-2">
@@ -62,6 +93,69 @@ function renderCamera() {
     `;
 
     cameraItems.appendChild(cameraItem);
+  });
+}
+
+function openCameraDetails(index) {
+  const camera = cameras[index];
+
+  $("#cameraDetailModal").modal("show");
+
+  const slidesHTML = camera.img
+    .map(
+      (imgSrc) => `
+        <div class="swiper-slide">
+            <img src="${imgSrc}" width="100%" />
+        </div>
+    `
+    )
+    .join("");
+
+  const contentSlider = document.getElementById("camera-swiper-slider");
+  const contentDetail = document.getElementById("camera-swiper-detail");
+
+  contentSlider.innerHTML = `
+  <div>
+                        <div class="swiper camera-swiper">
+                            <div class="swiper-wrapper">
+                                ${slidesHTML}
+                            </div>
+
+                            <div class="swiper-button-next"></div>
+                            <div class="swiper-button-prev"></div>
+                        </div>
+
+                        <div thumbsSlider="" class="swiper camera-swiper-thumbs">
+                            <div class="swiper-wrapper">
+                                                               ${slidesHTML}
+                            </div>
+                        </div>
+                    </div>
+  `;
+
+  contentDetail.innerHTML = `
+  <div>${camera.descDetails}</div>
+  `;
+
+  // Initialize Swiper instances
+  var swiperThumbs = new Swiper(".camera-swiper-thumbs", {
+    loop: true,
+    spaceBetween: 10,
+    slidesPerView: 5,
+    freeMode: true,
+    watchSlidesProgress: true,
+  });
+
+  var cameraSwiper = new Swiper(".camera-swiper", {
+    loop: true,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+      swiper: swiperThumbs,
+    },
   });
 }
 
